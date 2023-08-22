db708c947423d2b56f42ff1d5d5ca96e5ea16eb6
WiktorPospiech <wiktor.pospiech@gmail.com>
2023-08-22 11:32:49 +0200

    task-01 solution added

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,56 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+let instance;
+
+const photoGen = ({ preview, original, description }) => `
+  <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>
+`;
+
+const getImages = (img) => img.map((item) => photoGen(item)).join("");
+
+gallery.innerHTML = getImages(galleryItems);
+
+gallery.addEventListener("click", onClickGallery);
+
+const genBigImg = ({ url, description }) => `
+  <img
+    class="gallery__image"
+    src="${url}"
+    width="1280"
+    alt="${description}"/>
+`;
+
+function onClickGallery(e) {
+  e.preventDefault();
+
+  const url = e.target.dataset.source;
+  const description = e.target.alt;
+
+  if (!(url && description)) return;
+
+  instance = basicLightbox.create(genBigImg({ url, description }), {
+    onClose: () => {
+      gallery.removeEventListener("keydown", onKeyDownEsc);
+    },
+  });
+  instance.show();
+  gallery.addEventListener("keydown", onKeyDownEsc);
+}
+
+const onKeyDownEsc = (e) => {
+  if (e.key === "Escape" || e.keyCode === 27) {
+    instance.close();
+  }
+  console.log(e.key);
+};
