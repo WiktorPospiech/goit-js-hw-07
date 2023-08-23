a048428bb0fc8b6b71dc5e5de5f407e7c7706df5
WiktorPospiech <wiktor.pospiech@gmail.com>
2023-08-23 19:10:10 +0200

    task-02 solution added

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-consolevent.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const photoGen = ({ preview, original, description }) =>
+  `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>
+  </li>
+`;
+
+const getImages = (img) => img.map((item) => photoGen(item)).join("");
+
+gallery.innerHTML = getImages(galleryItems);
+
+gallery.addEventListener("click", onClickGallery);
+
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+function onClickGallery(e) {
+  e.preventDefault();
+}
+
+console.log(galleryItems);
